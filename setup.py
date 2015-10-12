fc40d37f2f066464218388ae21f788946b6d5c36
Apple <cjappl@cjappl-mbp.dolby.net>
2015-10-12 10:47:03 -0700

    Fixed setup.py to include pydub requirement.

--- setup.py
+++ setup.py
@@ -4,13 +4,24 @@
 # Last Edited: 10/12/15
 # Description: setup.py for installing custom packages
 # Run with command: python setup.py develop
-# ---------------------------------------------------- 
+# ----------------------------------------------------
 
+PROJECT_NAME = "Lido Project"
 
 from distutils.core import setup
 import setuptools
 
-setup(name='Lido',
-      version='1.0',
-      packages=['src/content', 'src/hardware']
+setup(name="Lido",
+      version="1.0",
+      description="Lido Project for KW, CA",
+      packages=["src/content", "src/hardware"],
+
+      zip_safe=False,
+
+      install_requires=[
+          "pydub>0.14.0"
+      ],
+
+      author="Chris Apple, Kaya Woodall",
+      author_email="christopher.j.apple@gmail.com, kwoodall@hmc.edu"
       )
